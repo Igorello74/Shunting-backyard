9ef2f37708956aa0aad94a4f314b47a1e7e09e1f
Igorello74 <sidneff.igor@ya.ru>
2021-09-24 00:43:41 +0500

    implement infix to postfix fully

--- shunting_yard.py
+++ shunting_yard.py
@@ -1,18 +1,26 @@
 from enum import Enum
 
+OPERATORS_PRIORITY = {
+    '+': 0, '-': 0, '/': 1, '*': 1, '^': 2
+}
+
+def get_operator_priority(op):
+    return OPERATORS_PRIORITY.get(op, -1)
+
+
 class Token:
     NUM = 0
     OPERATOR = 1
     VARIABLE = 2
+    BRACKET = 3
 
     def __init__(self, val, token_type):
         self.val = val
         self.token_type = token_type
     
-OPERATORS = "^*/+-"
 
 def shunting_yard(inp):
-    inp = inp.replace(' ', '')
+    inp = inp.replace(' ', '') # delete any spaces
 
     tokens = []
     prev = ""
@@ -25,11 +33,17 @@ def shunting_yard(inp):
                 tokens.append(Token(prev, Token.NUM))
                 prev = ''
                 
-            if i in OPERATORS:
+            if i in OPERATORS_PRIORITY:
                 tokens.append(Token(i, Token.OPERATOR))
             
             elif i.isalpha():
                 tokens.append(Token(i, Token.VARIABLE))
+            
+            elif i in '()':
+                tokens.append(Token(i, Token.BRACKET))
+            
+            else:
+                raise ValueError(f"Unknown character '{i}'")
             
     if prev:
         tokens.append(Token(prev, Token.NUM))
@@ -39,15 +53,28 @@ def shunting_yard(inp):
     operators_stack = []
 
     for i in tokens:
-        if i.token_type == Token.NUM:
+        if i.token_type in (Token.NUM, Token.VARIABLE):
             output.append(i.val)
+        
         elif i.token_type == Token.OPERATOR:
-            op_stack.append
-    return tokens
+            i_priority = get_operator_priority(i.val)
+            while operators_stack and \
+              get_operator_priority(operators_stack[-1]) >= i_priority:
+                output.append(operators_stack.pop())
+            operators_stack.append(i.val)
+            
+        elif i.val == '(':
+            operators_stack.append(i.val)
+        elif i.val == ')':
+            while operators_stack and operators_stack[-1] != '(':
+                output.append(operators_stack.pop())
+            operators_stack.pop()
+    
+    while operators_stack:
+        output.append(operators_stack.pop())
+    return ' '.join(output)
 
 
 if __name__ == '__main__':
     a = input()
-    b = shunting_yard(a)
-    for i in b:
-        print(f"{i.token_type}:\t{i.val}")
+    print(shunting_yard(a))
